d288bd95e5a32ef80728334903474adb82f1debd
Björn Hjorth <bjorn.hjorth@outlook.com>
2024-07-29 16:43:37 +0200

    adding setup

--- lessons/1/main.js
+++ lessons/1/main.js
@@ -1,12 +1,6 @@
+import Game from "@keoom/core/game.js"
 import config from "../../lessons/1/config.json" with { type: "json" }
 
-import Shape from "@keoom/components/shape.js"
-import Vector2 from "@keoom/core/vector2.js"
+const game = new Game(config)
 
-const v1 = new Vector2(2,3)
-const v2 = new Vector2(2,4)
-const shape = new Shape(2,3,4)
-
-v1.add(v2).subtract(v2).scale(v2)
-
-console.log(config)
+game.init() 
